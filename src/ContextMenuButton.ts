d34b6a0b6ba7a4e34e4dd8c72cea47637fd19372
David Severwright <code@davidsev.co.uk>
2024-07-28 19:59:21 +0100

    fix detecting the area for scaled maps

--- src/ContextMenuButton.ts
+++ src/ContextMenuButton.ts
@@ -1,13 +1,4 @@
-import OBR, {
-    buildShape,
-    ContextMenuContext,
-    ContextMenuIcon,
-    ContextMenuItem,
-    isImage,
-    isShape,
-    Item,
-    Vector2,
-} from '@owlbear-rodeo/sdk';
+import OBR, { buildShape, ContextMenuContext, ContextMenuIcon, ContextMenuItem, Item } from '@owlbear-rodeo/sdk';
 import getId from './getId';
 
 export class ContextMenuButton implements ContextMenuItem {
@@ -28,36 +19,6 @@ export class ContextMenuButton implements ContextMenuItem {
         },
     }];
 
-    private getItemSize (item: Item): Vector2 {
-        if (isImage(item)) {
-            return { x: item.image.width, y: item.image.height };
-        }
-        if (isShape(item)) {
-            return { x: item.width, y: item.height };
-        }
-        throw new Error('Unknown item type');
-    }
-
-    private getArea (items: Item[]): [Vector2, Vector2] {
-        let minX = Infinity;
-        let minY = Infinity;
-        let maxX = -Infinity;
-        let maxY = -Infinity;
-
-        for (const item of items) {
-            const size = this.getItemSize(item);
-            minX = Math.min(minX, item.position.x);
-            minY = Math.min(minY, item.position.y);
-            maxX = Math.max(maxX, item.position.x + size.x);
-            maxY = Math.max(maxY, item.position.y + size.y);
-        }
-
-        return [
-            { x: minX, y: minY },
-            { x: maxX - minX, y: maxY - minY },
-        ];
-    }
-
     async onClick (context: ContextMenuContext, elementId: string): Promise<void> {
 
         // Get the item we're working with.
@@ -75,14 +36,14 @@ export class ContextMenuButton implements ContextMenuItem {
         }
 
         // Calculate the size of the target item.
-        const [position, size] = this.getArea(context.items);
+        const area = await OBR.scene.items.getItemBounds(context.items.map(x => x.id));
 
         // Make a rectangle, move it to the bottom, and attach it to the map.
         const rect = buildShape()
             .zIndex(minZ - 1)
-            .position(position)
-            .width(size.x)
-            .height(size.y)
+            .position(area.min)
+            .width(area.width)
+            .height(area.height)
             .layer('MAP')
             .fillOpacity(0)
             .disableHit(true)
